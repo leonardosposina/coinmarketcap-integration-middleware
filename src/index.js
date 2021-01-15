92f52f5fc20099d2ffb455eb4f3f8bf15b53731d
Leonardo Sposina <leonardo.sposina@icloud.com>
2021-01-15 15:24:53 -0300

    feat: Route erro redirect to main page

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const api = require('./services/axios');
 
 const app = express();
 const port = process.env.PORT || 80;
 const corsOptions = { origin: process.env.CORS_ORIGIN };
-
-const welcome = require('./welcome-message.js');
-
-app.get('/', function(req, res) {
-  res.send(welcome);
-});
 
 app.get('/cryptocoins', cors(corsOptions), (req, res) => {
   let { start, limit, convert } = req.query;
@@ -38,6 +33,10 @@ app.get('/usage', cors(corsOptions), (req, res) => {
   api
     .get('/key/info')
     .then(response => res.json(response.data));
+});
+
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
 app.listen(port, () => {
